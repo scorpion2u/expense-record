45227e8ec35b3e0e66c42d96bbf93bec953d70c1
scorpion2u <anson2me@gmail.com>
2026-04-12 11:18:50 +0800

    创建 sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'expense-record-v1';
+const urlsToCache = [
+  '/expense-record/',
+  '/expense-record/index.html',
+  '/expense-record/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
