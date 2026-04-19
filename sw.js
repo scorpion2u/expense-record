5dd20c0d30e40c12ce3bda5bd8ef42c504a43628
scorpion2u <anson2me@gmail.com>
2026-04-19 18:24:07 +0800

    更新 sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,55 @@
-const CACHE_NAME = 'accounting-book-v1';
-const urlsToCache = [
+// 自动缓存版 sw.js - 适合网络不稳定的环境
+const CACHE_NAME = 'expense-cache-v2';
+
+// 需要确保离线时能返回页面的核心文件
+const CORE_FILES = [
   './',
   './index.html',
   './css/styles.css',
-  './js/constants.js',
-  './js/db.js',
-  './js/utils.js',
-  './js/modal.js',
-  './js/ui.js',
   './js/app.js',
   './js/events.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './js/ui.js',
+  './js/db.js',
+  './js/utils.js',
+  './js/constants.js',
+  './js/modal.js'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(CORE_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    })
+      .then(cachedResponse => {
+        // 如果有缓存，直接返回
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        // 没有缓存，发起网络请求
+        return fetch(event.request).then(response => {
+          // 请求成功，将响应克隆并存入缓存
+          if (response && response.status === 200) {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return response;
+        });
+      })
   );
 });
